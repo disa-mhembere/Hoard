0ff399afe7c1c95b25f4729050337a30a0b1052d
Disa Mhembere <dmhembe1@jhu.edu>
2018-09-06 03:15:13 -0400

    stop echos

--- src/source/wintls.cpp
+++ src/source/wintls.cpp
@@ -5,19 +5,19 @@
   www.hoard.org
 
   Author: Emery Berger, http://www.emeryberger.org
- 
+
   Copyright (c) 1998-2015 Emery Berger
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation; either version 2 of the License, or
   (at your option) any later version.
-  
+
   This program is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
-  
+
   You should have received a copy of the GNU General Public License
   along with this program; if not, write to the Free Software
   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
@@ -116,15 +116,15 @@ extern "C" {
   {
     static int np = HL::CPUInfo::computeNumProcessors();
     switch (fdwReason) {
-      
+
     case DLL_PROCESS_ATTACH:
       {
-	fprintf (stderr, versionMessage);
+	//fprintf (stderr, versionMessage);
 	InitializeWinWrapper();
 	getCustomHeap();
       }
       break;
-      
+
     case DLL_THREAD_ATTACH:
       if (np == 1) {
 	// We have exactly one processor - just assign the thread to
@@ -135,18 +135,18 @@ extern "C" {
       }
       getCustomHeap();
       break;
-      
+
     case DLL_THREAD_DETACH:
       {
 	// Dump the memory from the TLAB.
 	getCustomHeap()->clear();
-	
+
 #if USE_DECLSPEC_THREADLOCAL
 	auto * heap = threadLocalHeap;
 #else
 	auto * heap = (TheCustomHeapType *) TlsGetValue(LocalTLABIndex);
 #endif
-	
+
 	if (np != 1) {
 	  // If we're on a multiprocessor box, relinquish the heap
 	  // assigned to this thread.
@@ -155,11 +155,11 @@ extern "C" {
 
       }
       break;
-      
+
     case DLL_PROCESS_DETACH:
       FinalizeWinWrapper();
       break;
-      
+
     default:
       return TRUE;
     }
